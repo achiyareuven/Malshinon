6f58cc2f158b0d71569ee964769b3a0f6178c2be
achiya reuven <achiya3144@gmail.com>
2025-06-10 17:56:09 +0300

    Remove bin, obj and packages folders from tracking

--- Malshinon/Program.cs
+++ Malshinon/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Malshinon.Dal;
 
 namespace Malshinon
 {
@@ -10,6 +11,16 @@ namespace Malshinon
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine(PeopleDal.IsExists("o", "s"));
+            var person = PeopleDal.GetPersonByName("o", "s");
+            Console.WriteLine(person.Id);
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.SecretCode);
+            Console.WriteLine(person.NumMentions);
+            Console.WriteLine(person.NumReports);
+            ReprtsDal.AddReport(1, 2, "jhjjkhjkhjk");
         }
     }
 }
